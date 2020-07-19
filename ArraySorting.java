f4b0630984dd22098307945b556bb7a2ab72bd40
Logon <32345277+Logon27@users.noreply.github.com>
2020-07-19 14:58:34 -0400

    Update ArraySorting.java

--- ArraySorting.java
+++ ArraySorting.java
@@ -6,21 +6,26 @@ int pass = 0;
 // The statements in the setup() function 
 // execute once when the program begins
 void setup() {
-  size(640, 360);  // Size must be the first statement
+  //size(640, 360);  // Size must be the first statement
+  size(1280, 720);
   stroke(255);     // Set line drawing color to white
   frameRate(10);
-  
+
   float rectWidth = width / (float) rects.length;
-  for(int i = numberToSort-1; i >= 0; i--) {
-    rects[(numberToSort-1)-i] = new Rectangle(rectWidth, i+1);
+  for (int i = 0; i < rects.length; i++) {
+    rects[i] = new Rectangle(rectWidth, i+1);
   }
-  
+
+  //get the max value of the array.
+  //this is only really necessary if i randomize the values
   maxRectHeight = rects[0].getHeight();
-  for(int i = 0; i < rects.length; i++) {
-    if(rects[i].getHeight() > maxRectHeight) {
-       maxRectHeight = rects[i].getHeight();
+  for (int i = 0; i < rects.length; i++) {
+    if (rects[i].getHeight() > maxRectHeight) {
+      maxRectHeight = rects[i].getHeight();
     }
   }
+  
+  //shuffle the array
   shuffleArray(rects);
 }
 // The statements in draw() are executed until the 
@@ -29,48 +34,63 @@ void setup() {
 // line is executed again.
 void draw() { 
   background(0);   // Clear the screen with a black background
-  for(int i = 0; i < rects.length; i++) {
-     rects[i].display(i); 
+  for (int i = 0; i < rects.length; i++) {
+    rects[i].display(i);
   }
-  if(pass < rects.length - 1) {
-     selectionSortPass(pass);
-     pass++;
+  if (pass < rects.length - 1) {
+    selectionSortPass(pass);
+    pass++;
   } else {
-    //randomize the array 
+    //randomize the array
+    shuffleArray(rects);
+    pass = 0;
   }
 }
 
 void selectionSortPass(int i) {
-      // Find the minimum element in unsorted array 
-      int min_idx = i; 
-      for (int j = i+1; j < rects.length; j++) 
-          if (rects[j].getHeight() < rects[min_idx].getHeight()) 
-              min_idx = j; 
+  // Find the minimum element in unsorted array 
+  int min_idx = i; 
+  for (int j = i+1; j < rects.length; j++) 
+    if (rects[j].getHeight() < rects[min_idx].getHeight()) 
+      min_idx = j; 
 
-      // Swap the found minimum element with the first 
-      // element       
-      Rectangle temp = rects[min_idx];
-      rects[min_idx] = rects[i];
-      rects[i] = temp;
+  // Swap the found minimum element with the first 
+  // element       
+  Rectangle temp = rects[min_idx];
+  rects[min_idx] = rects[i];
+  rects[i] = temp;
+  
+  rects[min_idx].setChanging();
+  rects[i].setChanging();
 }
 
 class Rectangle {
   float rect_width;
   int rect_height; //this is the same as the rectangles "value"
-  
+  boolean changing = false;
+
   // Contructor
   Rectangle(float rect_width, int rect_height) {
     this.rect_width = rect_width;
     this.rect_height = rect_height;
   }
-  
+
   int getHeight() {
     return rect_height;
   }
   
+  void setChanging() {
+    this.changing = true; 
+  }
+
   // Custom method for drawing the object
   void display(int curr_index) {
-    fill(102);
+    if(changing) {
+      fill(color(255, 100, 10));
+      changing = false;
+    } else {
+      fill(102);
+    }
     //rect(x,y,width,height);
     float ratio = ((float) rect_height / (float) maxRectHeight);
     float rect_display_height = ratio * height;
@@ -78,16 +98,16 @@ class Rectangle {
   }
 }
 
-  void shuffleArray(Rectangle[] a) {
-      int n = a.length;
-      for (int i = 0; i < n; i++) {
-          int change = i + (int) random(n - i);
-          swap(a, i, change);
-      }
+void shuffleArray(Rectangle[] a) {
+  int n = a.length;
+  for (int i = 0; i < n; i++) {
+    int change = i + (int) random(n - i);
+    swap(a, i, change);
   }
+}
 
-  void swap(Rectangle[] a, int i, int change) {
-      Rectangle helper = a[i];
-      a[i] = a[change];
-      a[change] = helper;
-  }
+void swap(Rectangle[] a, int i, int change) {
+  Rectangle helper = a[i];
+  a[i] = a[change];
+  a[change] = helper;
+}
